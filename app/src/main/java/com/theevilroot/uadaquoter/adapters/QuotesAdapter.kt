a04b6dd5c30efbbba618ecb68ccef77c1202af39
TheEvilRoot <robot-creatix@ya.ru>
2018-08-26 14:00:57 +0300

    Created new quote dialog

--- app/src/main/java/com/theevilroot/uadaquoter/adapters/QuotesAdapter.kt
+++ app/src/main/java/com/theevilroot/uadaquoter/adapters/QuotesAdapter.kt
@@ -3,10 +3,12 @@ package com.theevilroot.uadaquoter.adapters
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Typeface
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import com.google.android.flexbox.JustifyContent
 import com.google.gson.GsonBuilder
@@ -49,78 +51,33 @@ open class QuotesAdapter: RecyclerView.Adapter<QuotesAdapter.QuoteViewHolder>() 
                 editedView.text = ""
             }
             itemView.setOnClickListener {
-                /**buildAlert(itemView.context,  {
-                    it.text = "Добавил: ${quote.adder}\nАвтор: ${quote.author}${if(quote.editedBy != null) "\nРедактировал:${quote.editedBy!!} ${dFormat.format(quote.editedAt)}" else "" }"
-                    it.textAlignment = TextView.TEXT_ALIGNMENT_TEXT_START
-                }, {
-                    it.text = quote.text
-                    it.textSize = it.textSize * 1.05f
-                    it.textAlignment = TextView.TEXT_ALIGNMENT_TEXT_START
-                }, {
-                    it.text = "Поделиться"
-                }, {
-                    it.text = "Редактировать"
-                }, {
-                    if (it) {
-                        QuoterAPI.shareQuote(itemView.context, quote)
-                    } else {
+                val view = LayoutInflater.from(itemView.context).inflate(R.layout.quote_dialog_layout, null, false)
+                val builder = AlertDialog.Builder(itemView.context, R.style.CustomAlert_Dialog).setView(view)
+                with(view) {
+                    val dAdderView = findViewById<TextView>(R.id.quote_dialog_adder)
+                    val dAuthorView = findViewById<TextView>(R.id.quote_dialog_author)
+                    val dEditorView = findViewById<TextView>(R.id.quote_dialog_editor)
+                    val dEditedAtView = findViewById<TextView>(R.id.quote_dialog_edited_at)
+                    val dTextView = findViewById<TextView>(R.id.quote_dialog_text)
+                    val dShareBtn = findViewById<ImageButton>(R.id.quote_dialog_share)
+                    val dEditBtn = findViewById<ImageButton>(R.id.quote_dialog_edit)
+                    val dEditedRoot = findViewById<View>(R.id.quote_dialog_edited_root)
+
+                    dAdderView.text = quote.adder
+                    dAuthorView.text = quote.author
+                    if (quote.editedBy != null) {
+                        dEditorView.text = quote.editedBy!!
+                        dEditedAtView.text = dFormat.format(quote.editedAt)
+                    } else dEditedRoot.visibility = View.GONE
+                    dTextView.text = quote.text
+                    dShareBtn.setOnClickListener { QuoterAPI.shareQuote(itemView.context, quote) }
+                    dEditBtn.setOnClickListener {
                         val intent = Intent(itemView.context, EditQuoteActivity::class.java)
                         intent.putExtra("quote", GsonBuilder().create().toJson(quote.toJson()))
                         itemView.context.startActivity(intent)
                     }
-                    true
-                }, autoShow = false, theme = R.style.AppTheme_Dialog_PopUp).apply {
-                    window.attributes.windowAnimations = R.style.AppTheme_Dialog_PopUp
-                    show()
-                }**/
-
-                AlertBuilder(itemView.context)
-                        .titleView {
-                            it.visibility = View.GONE
-                            ""
-                        }
-                        .textView { textView, _->
-                            textView.textAlignment = View.TEXT_ALIGNMENT_CENTER
-                            textView.setTextColor(itemView.context.getColor(android.R.color.white))
-                            textView.typeface = Typeface.create("sans-serif-condensed", Typeface.NORMAL)
-                            textView.text = "Добавил: ${quote.adder}\nАвтор: ${quote.author}${if(quote.editedBy != null) "\nРедактировал:${quote.editedBy!!} ${dFormat.format(quote.editedAt)}" else "" }"
-                            textView.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
-                            "quoteText"
-                        }
-                        .textView { it, _ ->
-                            it.textAlignment = View.TEXT_ALIGNMENT_CENTER
-                            it.setTextColor(itemView.context.getColor(android.R.color.white))
-                            it.typeface = Typeface.create("sans-serif-condensed", Typeface.NORMAL)
-                            it.text = quote.text
-                            it.textSize = it.textSize * 1.05f
-                            it.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
-                            "quoteText"
-                        }
-                        .buttonGroup(2, { it.justifyContent = JustifyContent.SPACE_AROUND }) { index, button, _ ->
-                            button.setBackgroundResource(android.R.color.transparent)
-                            button.typeface = Typeface.create("sans-serif-condensed", Typeface.NORMAL)
-                            button.setTextColor(itemView.context.getColor(android.R.color.white))
-                            button.textSize = 16f
-
-                            if(index == 0) {
-                                button.text = "Поделиться"
-                                button.setOnClickListener {
-                                    QuoterAPI.shareQuote(itemView.context, quote)
-                                }
-                                "shareBtn"
-                            } else {
-                                button.text = "Редактировать"
-                                button.setOnClickListener {
-                                    val intent = Intent(itemView.context, EditQuoteActivity::class.java)
-                                    intent.putExtra("quote", GsonBuilder().create().toJson(quote.toJson()))
-                                    itemView.context.startActivity(intent)
-                                }
-                                "editBtn"
-                            }
-                        }
-                        .windowAnimation(R.style.AppTheme_Dialog_PopUp)
-                        .autoShow(true)
-                        .build {  }
+                }
+                builder.create().apply { window?.attributes?.windowAnimations = R.style.AppTheme_Dialog_PopUp }.show()
             }
         }
     }
