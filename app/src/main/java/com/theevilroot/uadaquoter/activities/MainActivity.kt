02facb50d65d981a4ebfb54082693d98a86c0d59
TheEvilRoot <robot-creatix@ya.ru>
2018-11-11 15:22:34 +0300

    Removed search feature

--- app/src/main/java/com/theevilroot/uadaquoter/activities/MainActivity.kt
+++ app/src/main/java/com/theevilroot/uadaquoter/activities/MainActivity.kt
@@ -9,21 +9,16 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.PermissionChecker
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.theevilroot.uadaquoter.*
 import com.theevilroot.uadaquoter.adapters.QuotesAdapter
 import com.theevilroot.uadaquoter.adapters.SearchResultAdapter
-import com.theevilroot.uadaquoter.objects.IgnoreCaseButton
-import com.theevilroot.uadaquoter.objects.TextWatcherWrapper
 import com.theevilroot.uadaquoter.utils.bind
 import com.theevilroot.uadaquoter.utils.showAdderNameDialog
 
@@ -32,18 +27,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var app: App
     private lateinit var imm: InputMethodManager
     private lateinit var quotesAdapter: QuotesAdapter
-    private lateinit var searchAdapter: SearchResultAdapter
 
     private val toolbar by bind<Toolbar>(R.id.toolbar)
     private val quotesView by bind<RecyclerView>(R.id.quotes_view)
     private val loadingProcess by bind<ProgressBar>(R.id.progressBar)
     private val searchStatus by bind<TextView>(R.id.search_status)
-    private val searchLayout by bind<ConstraintLayout>(R.id.search_layout)
-    private val searchOverlayLayout by bind<ConstraintLayout>(R.id.search_overlay_layout)
-    private val searchClose by bind<ImageButton>(R.id.search_close)
-    private val searchIgnoreCase by bind<IgnoreCaseButton>(R.id.search_ignorcase)
-    private val searchField by bind<EditText>(R.id.search_field)
-    private val searchQuotesView by bind<RecyclerView>(R.id.search_list_view)
 
     private var localLoadingError: Boolean = false
     private var permissionGranted: Boolean = false
@@ -55,24 +43,10 @@ class MainActivity : AppCompatActivity() {
         app = application as App
         imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         quotesAdapter = QuotesAdapter()
-        searchAdapter = SearchResultAdapter { quote ->
-            val id = QuoterApi.quotes.indexOfFirst { it.id == quote.id }
-            quotesView.scrollToPosition(id)
-            closeSearch()
-        }
         quotesView.layoutManager = LinearLayoutManager(this)
-        searchQuotesView.layoutManager = LinearLayoutManager(this)
         quotesView.adapter = quotesAdapter
-        searchQuotesView.adapter = searchAdapter
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_window_close)
         supportActionBar!!.title = "Цитаты"
-        searchClose.setOnClickListener { closeSearch() }
-        searchIgnoreCase.setOnClickListener {
-            searchIgnoreCase.turnIgnoreCase()
-            onSearch(searchField.text.toString(), searchIgnoreCase.value)
-        }
-        searchOverlayLayout.setOnClickListener { closeSearch() }
-        searchField.addTextChangedListener(TextWatcherWrapper(onChange = { str, _, _, _ -> onSearch(str, searchIgnoreCase.value) }))
         if (PermissionChecker.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PermissionChecker.PERMISSION_GRANTED ||
                 PermissionChecker.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PermissionChecker.PERMISSION_GRANTED) {
             if(Build.VERSION.SDK_INT >= 23)
@@ -88,38 +62,6 @@ class MainActivity : AppCompatActivity() {
             } else {
                 onPermissionDenied()
             }
-        }
-    }
-
-    private fun onSearch(str: String, value: Boolean) {
-        if(str.isNotBlank()) {
-            when {
-                str.startsWith("#") ->
-                    searchAdapter.setQuotes(QuoterApi.quotes.filter { "#${it.id}" == str })
-                str.startsWith("+") -> {
-                    val q = str.substring(1).apply { if (!value) toLowerCase()  }
-                    searchAdapter.setQuotes(QuoterApi.quotes.filter {
-                        if (!value)
-                            it.adder.toLowerCase().contains(q)
-                        else it.adder.contains(q)
-                    })
-                }
-                str.startsWith("-") -> {
-                    val q = str.substring(1).apply { if (!value) toLowerCase()  }
-                    searchAdapter.setQuotes(QuoterApi.quotes.filter {
-                        if (!value)
-                            it.author.toLowerCase().contains(q)
-                        else it.author.contains(q)
-                    })
-                }
-                else -> searchAdapter.setQuotes(QuoterApi.quotes.filter {
-                    if (!value)
-                        it.text.toLowerCase().contains(str.toLowerCase())
-                     else it.text.contains(str)
-                })
-            }
-        } else {
-            searchAdapter.setQuotes(emptyList())
         }
     }
 
@@ -207,8 +149,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        if(!isSearchShowed())
-            menuInflater.inflate(R.menu.menu_toolbar_main, menu)
+        menuInflater.inflate(R.menu.menu_toolbar_main, menu)
         return true
     }
 
@@ -229,58 +170,11 @@ class MainActivity : AppCompatActivity() {
                     onPermissionDenied()
                 }
             }
-            R.id.tb_search -> {
-                showSearch()
-            }
             R.id.tb_add -> {
                startActivity(Intent(this, NewQuoteActivity::class.java))
             }
-            android.R.id.home -> {
-                closeSearch()
-            }
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    override fun onBackPressed() {
-        if (isSearchShowed()) {
-            closeSearch()
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    private fun showSearch() {
-        searchLayout.animate().alphaBy(0F).alpha(1F).setDuration(100).setUpdateListener {
-            if(it.animatedValue == 0F) {
-                searchLayout.visibility = View.VISIBLE
-                searchOverlayLayout.visibility = View.VISIBLE
-                invalidateOptionsMenu()
-            }
-            if(it.animatedValue == 1F) {
-                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.showSoftInput(searchField, InputMethodManager.SHOW_FORCED)
-            }
-        }.start()
-    }
-
-    private fun isSearchShowed(): Boolean =
-            searchLayout.visibility == View.VISIBLE
-
-    private fun closeSearch() {
-        with(currentFocus) {
-            if (this != null)
-                imm.hideSoftInputFromWindow(searchField.windowToken, 0)
-        }
-        searchLayout.animate().alphaBy(1F).alpha(0F).setDuration(100).setUpdateListener {
-            if(it.animatedValue == 1F) {
-                searchField.setText("")
-                searchStatus.visibility = View.GONE
-                searchLayout.visibility = View.GONE
-                searchOverlayLayout.visibility = View.GONE
-                invalidateOptionsMenu()
-            }
-        }.start()
     }
 
     private fun onPermissionGranted() {
